110a580f8025a9b20149d82ac9efb4cc473569bb
benat <benperez@ucm.es>
2022-03-23 18:52:42 +0100

    Junction and Road tables

--- Pr2v2/TrafficSimulatorTP/src/simulator/view/JunctionsTableModel.java
+++ Pr2v2/TrafficSimulatorTP/src/simulator/view/JunctionsTableModel.java
@@ -4,30 +4,60 @@ import java.util.List;
 
 import javax.swing.table.AbstractTableModel;
 
+import extra.jtable.EventEx;
 import simulator.model.Event;
 import simulator.model.RoadMap;
 import simulator.model.TrafficSimObserver;
 
 public class JunctionsTableModel extends AbstractTableModel implements TrafficSimObserver {
-
-	private String[] columnNames = { "Id", "Green", "Ques"};
-	
+	private List<EventEx> _events;
+	private String[] _colNames = { "Id", "Green", "Ques"};
+	public JunctionsTableModel() {
+		_events=null;	
+	}
+	public void update() {
+		// observar que si no refresco la tabla no se carga
+		// La tabla es la represantación visual de una estructura de datos,
+		// en este caso de un ArrayList, hay que notificar los cambios.
+		
+		// We need to notify changes, otherwise the table does not refresh.
+		fireTableDataChanged();//si cambio los datos del modelo al jtable hay q actualizarla, si no no se ven		
+	}//hay varios metodos para actualizar
+	public void setEventsList(List<EventEx> events) {
+		_events = events;
+		update();
+	}
+	public boolean isCellEditable(int row, int column) {//no puedes modificar el dato de la casilla
+		return false;
+	}
+	public String getColumnName(int col) {
+		return _colNames[col];
+	}
 	@Override
 	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return _events == null ? 0 : _events.size();//no puedo hacer null.size()
 	}
 
 	@Override
 	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return _colNames.length;
 	}
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		Object s = null;
+		switch(columnIndex) {
+		case 0:
+			s=_events.get(rowIndex).toString().valueOf("id");
+			break;
+		case 1:
+			s=_events.get(rowIndex).toString().valueOf("Green");
+			break;
+		case 2:
+			s=_events.get(rowIndex).toString().valueOf("Queues");
+			break;
+		}
+		return s;
 	}
 
 	@Override
--- Pr2v2/TrafficSimulatorTP/src/simulator/view/RoadsTableModel.java
+++ Pr2v2/TrafficSimulatorTP/src/simulator/view/RoadsTableModel.java
@@ -18,33 +18,69 @@ public class RoadsTableModel extends AbstractTableModel implements TrafficSimObs
 	        "Max. Speed", "Speed Limit", "Total CO2", "CO2 Limit" };
 
 	public RoadsTableModel() {
-		_events=null;
+		_events=null;	
 	}
 
-	  
+	public void update() {
+		// observar que si no refresco la tabla no se carga
+		// La tabla es la represantación visual de una estructura de datos,
+		// en este caso de un ArrayList, hay que notificar los cambios.
+		
+		// We need to notify changes, otherwise the table does not refresh.
+		fireTableDataChanged();//si cambio los datos del modelo al jtable hay q actualizarla, si no no se ven		
+	}//hay varios metodos para actualizar
 
+	public void setEventsList(List<EventEx> events) {
+		_events = events;
+		update();
+	}
+	
+	public boolean isCellEditable(int row, int column) {//no puedes modificar el dato de la casilla
+		return false;
+	}
 	
 	
-	
-	
-	
-	
+	public String getColumnName(int col) {
+		return _colNames[col];
+	}
+
 	@Override
 	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return _events == null ? 0 : _events.size();//no puedo hacer null.size()
+
 	}
 
 	@Override
 	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return _colNames.length;
 	}
-
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		Object s = null;
+		switch(columnIndex) {
+		case 0:
+			s=_events.get(rowIndex).toString().valueOf("id");
+			break;
+		case 1:
+			s=_events.get(rowIndex).toString().valueOf("length");
+			break;
+		case 2:
+			s=_events.get(rowIndex).toString().valueOf("weather");
+			break;
+		case 3:
+			s=_events.get(rowIndex).toString().valueOf("MaxSpeed");
+			break;
+		case 4:
+			s=_events.get(rowIndex).toString().valueOf("SpeedLimit");
+			break;
+		case 5:
+			s=_events.get(rowIndex).toString().valueOf("totalCO2");
+			break;
+		case 6:
+			s=_events.get(rowIndex).toString().valueOf("CO2Limit");
+			break;	
+		}
+		return s;
 	}
 
 	@Override
